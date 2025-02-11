347a0a986505df83a7e3e1c240919d16f1a68a48
Javibu13 <javiersanzmoline@gmail.com>
2025-02-11 19:43:09 +0100

    First sample version from internet tutorial to test github pages

---
+++ web/code.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three'
+import { MindARThree } from 'mindar-image-three'
+const mindarThree = new MindARThree({
+  container: document.querySelector('#container'),
+  imageTargetSrc: './targets.mind'
+})
+const { renderer, scene, camera } = mindarThree
+const anchor = mindarThree.addAnchor(0)
+const geometry = new THREE.PlaneGeometry(1, 0.55)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: false, opacity: 0.5 })
+const plane = new THREE.Mesh(geometry, material)
+anchor.group.add(plane)
+
+async function startAR () {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+    await mindarThree.start()
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera)
+    })
+  } catch (error) {
+    console.error('Error accessing the camera', error)
+  }
+}
+
+const startButton = document.querySelector('#startButton')
+const stopButton = document.querySelector('#stopButton')
+startButton.addEventListener('click', () => {
+  startAR()
+})
+stopButton.addEventListener('click', () => {
+  mindarThree.stop()
+  mindarThree.renderer.setAnimationLoop(null)
+})
